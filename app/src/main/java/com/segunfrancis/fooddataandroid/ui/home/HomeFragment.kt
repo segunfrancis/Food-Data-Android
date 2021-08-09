fcce51e865838ec27b51d6349e6c3529144f2ca0
Segun Francis <francis4dblues@gmail.com>
2021-08-09 09:15:24 +0100

    * Display data in a list

--- app/src/main/java/com/segunfrancis/fooddataandroid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/segunfrancis/fooddataandroid/ui/home/HomeFragment.kt
@@ -3,48 +3,76 @@ package com.segunfrancis.fooddataandroid.ui.home
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.segunfrancis.fooddataandroid.databinding.FragmentHomeBinding
 import com.segunfrancis.fooddataandroid.FoodDataApplication
+import com.segunfrancis.fooddataandroid.R
+import com.segunfrancis.fooddataandroid.util.Result
 import timber.log.Timber
 import javax.inject.Inject
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
 
     @Inject
     lateinit var factory: HomeViewModelFactory
 
     private val viewModel: HomeViewModel by viewModels { factory }
 
+    private val homeAdapter: HomeRecyclerAdapter by lazy { HomeRecyclerAdapter() }
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         (requireActivity().application as FoodDataApplication).appComponent.inject(this)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentHomeBinding.inflate(layoutInflater)
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+        _binding = binding
+
+        setupList()
+    }
+
+    private fun setupList() {
+        binding.itemsRecyclerView.apply {
+            adapter = homeAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
     }
 
     override fun onResume() {
         super.onResume()
 
         viewModel.foodResponse.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is Result.Success -> {
+                    homeAdapter.submitList(result.data)
+                    binding.text.visibility = View.GONE
+                }
+                is Result.Loading -> {
+                    binding.text.apply {
+                        visibility = View.VISIBLE
+                        text = "Loading..."
+                    }
+                }
+                is Result.Error -> {
+                    binding.text.apply {
+                        visibility = View.VISIBLE
+                        text = result.error.localizedMessage
+                    }
+                }
+            }
             Timber.d(result.toString())
         }
     }
 
     override fun onDestroyView() {
-        super.onDestroyView()
         _binding = null
+        super.onDestroyView()
     }
 }
---
+++ app/src/main/java/com/segunfrancis/fooddataandroid/ui/home/HomeRecyclerAdapter.kt
@@ -0,0 +1,44 @@
+package com.segunfrancis.fooddataandroid.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.segunfrancis.fooddataandroid.R
+import com.segunfrancis.fooddataandroid.databinding.ItemFoodHomeBinding
+import com.segunfrancis.fooddataandroid.model.BaseResponseItemApp
+
+class HomeRecyclerAdapter : ListAdapter<BaseResponseItemApp, HomeRecyclerAdapter.HomeViewHolder>(HomeDiffUtil()) {
+
+    class HomeViewHolder(private val binding: ItemFoodHomeBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(itemApp: BaseResponseItemApp) = with(binding) {
+            descriptionText.text = itemApp.description
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_food_home, parent, false)
+        return HomeViewHolder(ItemFoodHomeBinding.bind(view))
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class HomeDiffUtil : DiffUtil.ItemCallback<BaseResponseItemApp>() {
+        override fun areItemsTheSame(
+            oldItem: BaseResponseItemApp,
+            newItem: BaseResponseItemApp
+        ): Boolean {
+            return oldItem.fdcId == newItem.fdcId
+        }
+
+        override fun areContentsTheSame(
+            oldItem: BaseResponseItemApp,
+            newItem: BaseResponseItemApp
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
